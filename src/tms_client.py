c9e5d2fb988af300588a17fd678738ae6f979791
investmentmanagement137 <investmentmanagement137@gmail.com>
2026-01-18 04:59:10 +0545

    Fix: Add missing beautifulsoup4 dependency and import

--- src/tms_client.py
+++ src/tms_client.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from .utils import get_tms_number
+from bs4 import BeautifulSoup
 
 class TMSClient:
     def __init__(self, driver):
